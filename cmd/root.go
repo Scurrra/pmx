5b9f4c9223e2463838b7b0715dee3207736c54ef
Scurrra <iscurrra@gmail.com>
2023-07-20 19:13:38 +0300

    Oh sh*t, here we go again

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pmx",
+	Short: "Project manager for C/C++.",
+	Long: `
+Project manager for C/C++, that helps you to create, document, test and deliver 
+binary applications and libraries. 
+	`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
